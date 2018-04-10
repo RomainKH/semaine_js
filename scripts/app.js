a80899e559df4756233aeeaa13d931cca4592a3c
RomainKH <romain.khanoyan@gmail.com>
2018-04-10 22:44:10 +0200

    favicon + js

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,18 @@
-let premier = new Array(), a=0, i = 1
+const agesOne = document.getElementsByClassName('fiveEight')
+const agesTwo = document.getElementsByClassName('nineEleven')
+const agesThree = document.getElementsByClassName('twelveUp')
 
-while(premier.length <= 20)
-{
-  for (let j = 2;j<i;j++)
-  {
-    if (i%j == 0)
-    (
-      a++
-    )
-  }
-  if (a==0)
-  {
-    premier.push(i)
-  }
-  a = 0
-  i++
+function loadPage(href){
+  let xmlhttp = new XMLHttpRequest()
+  xmlhttp.open("GET", href, false)
+  xmlhttp.send()
+  return xmlhttp.responseText
 }
-
-console.log(premier)
+for (var i = 0; i < agesOne.length; i++) {
+  agesOne[i].addEventListener(
+    "click",
+    function(){
+      loadPage('pages/game-child.html')
+    }
+  )
+}
