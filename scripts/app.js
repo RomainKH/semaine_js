b78364d20161b67bd677918a2a2ffb06157d1b1f
RomainKH <romain.khanoyan@gmail.com>
2018-04-12 15:43:52 +0200

    js local

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,58 @@
-const boutonUn = document.querySelector('#category div .un')
-const boutonDeux = document.querySelector('#category div .deux')
-const boutonTrois = document.querySelector('#category div .trois')
+let boutonUn = document.querySelector('#category div .un'),
+    boutonDeux = document.querySelector('#category div .deux'),
+    boutonTrois = document.querySelector('#category div .trois')
 
-let category = document.querySelector('#category'),
-    themes = category.querySelectorAll('div'),
-    categories = []
 
-for (var i = 0; i < themes.length; i++) {
+let select = document.querySelector('#age form select')
+let varSelect = new Array ()
+let jeune = "jeune"
+let vieux = "vieux"
+let localAge=localStorage.getItem('localAge')
+if (select !== null) {
 
-  categories.push(themes[i].getAttribute('id'))
+  select.addEventListener(
+    "change",
+    function() {
+      let userChoice = select.options[select.selectedIndex].value
+      location.href = "pages/home_page.html"
+      localStorage.setItem('localAge', userChoice)
+      console.log(userChoice)
+    })
 }
+console.log(localAge)
 
-boutonUn.addEventListener(
-  "click",function(){
-    categories = JSON.stringify(categories[0])
-    localStorage.setItem('localThemes', categories)
+
+
+  let category = document.querySelector('#category'),
+      themes = category.querySelectorAll('div'),
+      categories = []
+
+  for (var i = 0; i < themes.length; i++) {
+
+    categories.push(themes[i].getAttribute('id'))
   }
-)
 
-boutonDeux.addEventListener(
-  "click",function(){
-    categories = JSON.stringify(categories[1])
-    localStorage.setItem('localThemes', categories)
-  }
-)
+  boutonUn.addEventListener(
+    "click",function(){
+      categories = JSON.stringify(categories[0])
+      localStorage.setItem('localThemes', categories)
+    }
+  )
 
-boutonTrois.addEventListener(
-  "click",function(){
-    categories = JSON.stringify(categories[2])
-    localStorage.setItem('localThemes', categories)
-  }
-)
+  boutonDeux.addEventListener(
+    "click",function(){
+      categories = JSON.stringify(categories[1])
+      localStorage.setItem('localThemes', categories)
+    }
+  )
+
+  boutonTrois.addEventListener(
+    "click",function(){
+      categories = JSON.stringify(categories[2])
+      localStorage.setItem('localThemes', categories)
+    }
+  )
 
 
-let localThemes = localStorage.getItem('localThemes')
-console.log(localThemes)
-
-console.log("Catégories: ", categories)
-console.log(categories[1])
+  let localThemes = localStorage.getItem('localThemes')
+  console.log(localThemes)
