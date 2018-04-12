99b9ce0483ae75d89996a98fe1c5039f718c9fed
RomainKH <romain.khanoyan@gmail.com>
2018-04-12 20:58:12 +0200

    push

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,28 @@
-let headDb=['../images/oeufs.svg','../images/poivron.svg','../images/pomme.svg','../images/bananes.svg','../images/chocolat.svg','../images/citron.svg','../images/fromage.svg','../images/pates.svg','../images/pain.svg','../images/riz.svg','../images/raisin.svg','../images/orange.svg','../images/concombre.svg','../images/viande.svg','../images/tomate.svg','../images/salade.svg','../images/carotte.svg','../images/radis.svg','../images/pizza.svg','../images/hamburger.svg','../images/poisson.svg','../images/fraise.svg','../images/broccoli.svg','../images/poire.svg','../images/ananas.svg','../images/poulet.svg','../images/oignon.svg']
-let headDbName=['oeufs','poivrons','pomme','banane','chocolat','citron','fromage','pâtes','pain','riz','raisin','orange','concombre','viande hachée','tomate','salade','carotte','radis','pizza','hamburger','poisson','fraise','broccoli','poire','ananas','poulet','oignon']
-let headDescriptionDb=['pas_vegan rond protéines','pousse_terre légume peau rouge_jaune','fruit jus peau sucré arbre rond pépins rouge_jaune','fruit peau sucré arbre rouge_jaune','transformé sucré','fruit jus peau arbre rond pépins rouge_jaune','transformé pas_vegan protéines','féculent rouge_jaune','transformé féculent','pousse_terre féculent','fruit jus peau sucré arbre rond pépins','fruit jus peau sucré arbre rond pépins','pousse_terre légume peau','transformé pas_vegan protéines','fruit jus peau rond rouge_jaune','pousse_terre légume','pousse_terre légume peau','pousse_terre légume','transformé pas_vegan rond','transformé pas_vegan','pas_vegan peau protéines','fruit jus sucré rouge_jaune','pousse_terre','fruit peau sucré pépins arbre','fruit jus peau sucré','pas_vegan peau protéines','pousse_terre légume peau rond']
-let questionsDb=["Est-ce que l'aliment est un fruit ?","Est-ce que l'aliment est transformé ?","Est-ce que ça pousse dans la terre ?","Est-ce que l'aliment n'est pas vegan ?","Est-ce que c'est un légume ?","Est-ce que c'est un fruit à jus ?","Est-ce que l'aliment a une peau ?","Est-ce que c'est un féculent ?","Est-ce que c'est un aliment riche en protéines ?","Est-ce que l'aliment est sucré ?","Est-ce que cet aliment pousse dans un arbre ?","Est-ce que ça a une forme ronde ?","Est-ce que l'aliment a des pépins ?","Est-ce que ça a une couleur rouge ou jaune ?"]
-let questionsDescriptionDb=["fruit","transformé","pousse_terre","pas_vegan","légume","jus","peau","féculent","protéines","sucré","arbre","rond","pépins","rouge_jaune"]
+let localThemes = localStorage.getItem('localThemes')
+if (localThemes == animaux) {
+  let animalsName = animals.
+}
+else if (localThemes == sports) {
+  let sportsName = sports.
+}
+else if (localThemes == alimentation) {
+
+}
+else if (localThemes == cinema) {
+
+}
+else if (localThemes == pays) {
+
+}
+else if (localThemes == rois) {
+
+}
+
+let headDb=[]
+let headDbName=[]
+let headDescriptionDb=[]
+let questionsDb=[]
+let questionsDescriptionDb=[]
 let headPlayZone=document.getElementById('play')
 let errorNumber=0
 let cardsCount=0
